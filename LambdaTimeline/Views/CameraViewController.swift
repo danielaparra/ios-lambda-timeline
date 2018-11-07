636f219003596bf61d1a65f1ca5b6c9df9c293e7
Daniela Parra <dyparra47@gmail.com>
2018-11-07 16:57:31 -0500

    Add video view controller.

---
+++ LambdaTimeline/Views/CameraViewController.swift
@@ -0,0 +1,9 @@
+//
+//  CameraViewController.swift
+//  LambdaTimeline
+//
+//  Created by Daniela Parra on 11/7/18.
+//  Copyright © 2018 Lambda School. All rights reserved.
+//
+
+import Foundation
